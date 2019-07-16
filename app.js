ea3f4595a22bd2d59a31e7323fd8c0ca9f2c52e5
Luca Grotto <grottoluca@gmail.com>
2019-07-16 22:03:50 +0200

    Add helper function for rootDir

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const adminRoute = require('./routes/admin');
@@ -10,7 +11,7 @@ app.use('/admin', adminRoute);
 app.use(shopRoute);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found</h1>')
+    res.status(404).sendFile(path.join(__dirname, 'view', '404.html'))
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,11 @@
 const path = require('path');
 const express = require('express');
 const router = express.Router();
+const rootDir = require('../util/path');
 
 router.get('/', (req, res, next) => {
     console.log('In the middleware')
-    res.sendFile(path.join(__dirname, '../', 'view' , 'shop.html'));
+    res.sendFile(path.join(rootDir, 'view' , 'shop.html'));
 });
 
 module.exports = router;
