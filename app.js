1476244c3d9d6cd8596072e0861cc1173453a944
Luca Grotto <grottoluca@gmail.com>
2019-07-20 11:13:49 +0200

    Add ejs template engine

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const adminData = require('./routes/admin');
 const shopRoute = require('./routes/shop');
 
 const app = express();
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 app.use(bodyParser.urlencoded());
 
@@ -15,7 +15,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoute);
 
 app.use((req, res, next) => {
-    res.status(404).render('404')
+    res.status(404).render('404', {docTitle: "Page not found"})
 });
 
 app.listen(3000);
