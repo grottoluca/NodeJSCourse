4f37c03fde63bef5d9ae69224f78cfc607d2a9ac
Luca Grotto <grottoluca@gmail.com>
2019-07-27 12:09:28 +0200

    Add editing and deliting product

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
@@ -9,6 +10,17 @@ exports.getProducts = (req, res, next) => {
     });
   });
 };
+
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+      res.render('shop/product-detail', {
+        pageTitle: product.title, 
+        product: product,
+        path: '/product'
+      })
+  });
+}
 
 exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
@@ -25,6 +37,15 @@ exports.getCart = (req, res, next) => {
     path: '/cart',
     pageTitle: 'Your Cart'
   });
+};
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  console.log(prodId);
+  Product.findById(prodId, (product) => {
+    Cart.addProduct(prodId, product.price);
+  })
+  res.redirect('/cart')
 };
 
 exports.getOrders = (req, res, next) => {
--- routes/admin.js
+++ routes/admin.js
@@ -15,4 +15,11 @@ router.get('/products', adminController.getProducts);
 // /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
 
+// /admin/edit-product => GET
+router.get('/edit-product/:productId', adminController.getEditProduct);
+
+router.post('/delete-product/', adminController.postDeleteProduct);
+
+router.post('/edit-product', adminController.postEditProduct);
+
 module.exports = router;
