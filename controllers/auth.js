d11c7b0d82223bc0770eee442487f4d6e18989c3
Luca Grotto <grottoluca@gmail.com>
2019-08-23 17:54:52 +0200

    Add auth method and send email feature

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,35 +1,111 @@
+const crypt = require('bcryptjs');
+const nodemailer = require('nodemailer');
+const sendGridTransport = require('nodemailer-sendgrid-transport');
+
 const User = require('../models/user');
 
+const transporter = nodemailer.createTransport(sendGridTransport({
+  auth: {
+    api_key: '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'
+  }
+}));
+
 exports.getLogin = (req, res, next) => {
+  let message = req.flash('error');
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: false
+    isAuthenticated: false,
+    errorMessage: message
   });
 };
 
 exports.getSignup = (req, res, next) => {
+  let message = req.flash('error');
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
   res.render('auth/signup', {
     path: '/signup',
     pageTitle: 'Signup',
-    isAuthenticated: false
+    isAuthenticated: false,
+    errorMessage: message
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ email: email })
     .then(user => {
-      req.session.isLoggedIn = true;
-      req.session.user = user;
-      req.session.save(err => {
-        console.log(err);
-        res.redirect('/');
-      });
+      if (!user) {
+        req.flash('error', 'Invalid email or password.')
+        return res.redirect('login');
+      }
+      crypt.compare(password, user.password)
+        .then(doMatch => {
+          if (doMatch) {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            return req.session.save(err => {
+              console.log(err);
+              res.redirect('/');
+            });
+          }
+          req.flash('error', 'Invalid email or password.')
+          res.redirect('login');
+        }).catch(err => {
+          res.redirect('login');
+        })
     })
     .catch(err => console.log(err));
 };
 
-exports.postSignup = (req, res, next) => {};
+exports.postSignup = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+
+  User.findOne({ email: email })
+    .then(userDoc => {
+      if (userDoc) {
+        req.flash('error', 'Email already exist.')
+        return res.redirect('/signup');
+      }
+      return crypt.hash(password, 12)
+        .then(hashedPassoword => {
+          const user = new User({
+            email: email,
+            password: hashedPassoword,
+            cart: { items: [] }
+          });
+          return user.save();
+        })
+        .then(result => {
+          res.redirect('login');
+          return transporter.sendMail({
+            to: email,
+            from: 'shop@node.com',
+            subject: 'Registration succeded',
+            html: '<h1>You successfully sign up!</h1>'
+          });
+        })
+        .catch(err => {
+          console.log(err);
+        })
+    })
+    .catch(err => {
+      console.log(err);
+    });
+
+};
 
 exports.postLogout = (req, res, next) => {
   req.session.destroy(err => {
